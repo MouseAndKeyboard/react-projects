4f77a3a3beefc58a9ded29076a2de06e47a6a5b9
Michael Nef <michael@nef.net.au>
2020-11-03 10:43:55 +0800

    menu project

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filter, categories}) => {
+  const categoryButtons = categories.map((category, index) => (
+    <button key={index} className="filter-btn" onClick={() => filter(category)}>
+      {category}
+    </button>
+  ));
+
+  return (
+    <div className="btn-container">
+      <button onClick={() => filter("all")} className="filter-btn">
+        All
+      </button>
+      {categoryButtons}
+    </div>
+  );
 };
 
 export default Categories;
--- 5-menu/setup/src/Menu.js
+++ 5-menu/setup/src/Menu.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({items}) => {
+  const menuItems = items.map((item) => (
+    <div key={item.id} className="menu-item">
+      <img className="photo" alt={item.title} src={item.img} />
+      <div className="item-info">
+        <header>
+          <h4>{item.title}</h4>
+          <h4 className="price">{item.price}</h4>
+        </header>
+
+        <p className="item-text">{item.desc}</p>
+      </div>
+    </div>
+  ));
+
+  return <div className="section-center">{menuItems}</div>;
 };
 
 export default Menu;
