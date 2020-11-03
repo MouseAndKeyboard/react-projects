72c7f356d3d31d95415370a6878bd2b5a5103cce
Michael Nef <michael@nef.net.au>
2020-11-03 09:58:14 +0800

    accordian project

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, {useState} from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  const questionsComponents = questions.map((question) => {
+    return <SingleQuestion key={question.id} {...question} />;
+  });
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions And Answers About Login</h3>
+        <section className="info">{questionsComponents}</section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, {useState} from "react";
+import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
+const Question = ({title, info}) => {
+  const [open, setOpen] = useState(false);
+
+  const expandIcon = open ? <AiOutlineMinus /> : <AiOutlinePlus />;
+  const infoContent = open ? <p>{info}</p> : <></>;
+
+  return (
+    <div className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setOpen(!open)}>
+          {expandIcon}
+        </button>
+      </header>
+      {infoContent}
+    </div>
+  );
 };
 
 export default Question;
